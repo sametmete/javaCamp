e51a5cf90cd0dfaacc22a1a89dff74b9b2ec2f85
sametmete <75644552+sametmete@users.noreply.github.com>
2021-05-16 23:29:23 +0300

    Add files via upload

---
+++ MernisDatabaseKayit/src/Entities/Customer.java
@@ -0,0 +1,76 @@
+package Entities;
+
+import java.time.LocalDate;
+
+import Abstract.Entity;
+
+
+
+public class Customer implements Entity {
+
+	
+	private int Id;
+	private String FirstName;
+	private String LastName;
+	private LocalDate DateOfBirth;
+	private String NationalityId;
+	
+	public Customer() {
+		
+	}
+
+	
+
+	public Customer(int id, String firstName, String lastName, LocalDate dateOfBirth, String nationalityId) {
+		this();
+		this.Id = id;
+		this.FirstName = firstName;
+		this.LastName = lastName;
+		this.DateOfBirth = dateOfBirth;
+		this.NationalityId = nationalityId;
+	}
+
+
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return DateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		DateOfBirth = dateOfBirth;
+	}
+
+	public String getNationalityId() {
+		return NationalityId;
+	}
+
+	public void setNationalityId(String nationalityId) {
+		NationalityId = nationalityId;
+	}
+	
+	
+}
